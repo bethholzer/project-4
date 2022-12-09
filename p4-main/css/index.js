84447b04657fe542f9f396f4951a83ba5558642e
bethholzer <98134165+bethholzer@users.noreply.github.com>
2022-12-09 14:17:09 -0800

    Add files via upload

---
+++ p4-main/css/index.js
@@ -0,0 +1,45 @@
+function startTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('marquee').innerHTML =  h + ":" + m + ":" + s;
+    document.getElementById('marquee-2').innerHTML =  h + ":" + m + ":" + s;
+    setTimeout(startTime, 1000);
+  }
+  
+  function checkTime(i) {
+    if (i < 10) {i = "0" + i};  
+    return i;
+  }
+
+
+  setInterval(() => {
+    d = new Date(); //object of date()
+    hr = d.getHours();
+    min = d.getMinutes();
+    sec = d.getSeconds();
+    hr_rotation = 30 * hr + min / 2; //converting current time
+    min_rotation = 6 * min;
+    sec_rotation = 6 * sec;
+  
+    hour.style.transform = `rotate(${hr_rotation}deg)`;
+    minute.style.transform = `rotate(${min_rotation}deg)`;
+    second.style.transform = `rotate(${sec_rotation}deg)`;
+}, 1000);
+
+let images = ['img/riso-1.gif', 'img/riso-2.gif', 'img/riso-3.gif' , 'img/riso-4.gif' , 'img/riso-5.gif'];
+
+let index = 0;
+const imgElement = document.querySelector('#riso-1');
+
+function change() {
+   imgElement.src = images[index];
+   index > 1 ? index = 0 : index++;
+}
+
+window.onload = function () {
+    setInterval(change, 5000);
+};
